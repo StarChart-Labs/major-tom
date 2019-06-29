27dbba511980a8260c84065fc699856f7b55bcb4
Ryan O'Meara <romeara@live.com>
2019-06-28 23:09:23 -0400

    Basic working post to Slack in response to received event

--- event-handler/src/main/java/org/starchartlabs/majortom/event/handler/aws/Handler.java
+++ event-handler/src/main/java/org/starchartlabs/majortom/event/handler/aws/Handler.java
@@ -10,16 +10,38 @@
  */
 package org.starchartlabs.majortom.event.handler.aws;
 
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.starchartlabs.alloy.core.MoreObjects;
+import org.starchartlabs.alloy.core.Suppliers;
+import org.starchartlabs.machete.ssm.parameter.StringParameter;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Handler implements RequestHandler<SNSEvent, Void> {
 
     private static final String PARAMETER_STORE_SLACK_URL = "SLACK_URL_SSM";
+
+    private static final Supplier<String> SLACK_URL_SUPPLIER = Suppliers
+            .memoizeWithExpiration(StringParameter.fromEnv(PARAMETER_STORE_SLACK_URL), 10, TimeUnit.MINUTES);
 
     /** Logger reference to output information to the application log files */
     private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -29,7 +51,78 @@ public class Handler implements RequestHandler<SNSEvent, Void> {
         // TODO make trace
         logger.info("Received SNS event: " + input);
 
+        postMessage("Hello World!");
+
         return null;
+    }
+
+    private void postMessage(String text) {
+        Message message = new Message(text);
+
+        OkHttpClient httpClient = new OkHttpClient();
+
+        HttpUrl url = HttpUrl.get(SLACK_URL_SUPPLIER.get());
+
+        Request request = new Request.Builder()
+                .header("User-Agent", "StarChart-Labs/Major-Tom")
+                .post(RequestBody.create(MediaType.get("application/json"), message.toJson()))
+                .url(url)
+                .build();
+
+        try {
+            Response response = httpClient.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("Failed to POST status (" + response.code() + ")");
+            }
+        } catch (IOException e) {
+            // TODO romeara Auto-generated catch block
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final class Message {
+
+        private static final Gson GSON = new GsonBuilder().create();
+
+        private final String text;
+
+        public Message(String text) {
+            this.text = Objects.requireNonNull(text);
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String toJson() {
+            return GSON.toJson(this);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getText());
+        }
+
+        @Override
+        public boolean equals(@Nullable Object obj) {
+            boolean result = false;
+
+            if (obj instanceof Handler.Message) {
+                Handler.Message compare = (Handler.Message) obj;
+
+                result = Objects.equals(compare.getText(), getText());
+            }
+
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return MoreObjects.toStringHelper(getClass()).omitNullValues()
+                    .add("text", getText())
+                    .toString();
+        }
     }
 
 }
